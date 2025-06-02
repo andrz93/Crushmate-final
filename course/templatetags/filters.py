d4a021970eea1c0ce748e795d0e8ec8f2b397954
pikochung <t111310100@ntut.org.tw>
2025-06-02 19:03:26 +0800

    biocourse

--- course/templatetags/filters.py
+++ course/templatetags/filters.py
@@ -1,11 +1,22 @@
-# templatetags/filters.py
 from django import template
 register = template.Library()
 
 @register.filter
-def get_cell(table_data, key):
-    return table_data.get(key, '')
+def index(sequence, i):
+    try:
+        return sequence[int(i) - 1]  # 如果 periods 是從 1 開始
+    except:
+        return ""
+@register.filter
+def get_course(day_dict, i):
+    try:
+        return day_dict[int(i) - 1]
+    except:
+        return ""
 
 @register.filter
-def get_cell(dictionary, key):
-    return dictionary.get(key, "")
+def get_course_data(data, key):
+    try:
+        return data.get(key, [])
+    except:
+        return []
